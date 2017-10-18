d719755c647b92ad218e8963b292c8f92f66f839
jonathan <jonathan.palomino.vilca@gmail.com>
2017-10-18 16:53:05 -0500

    spring 4 arquetipo

---
+++ spring-mvc4/src/main/java/com/palomino/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.palomino.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@ComponentScan(basePackages = "com.palomino")
+public class ApplicationConfig {
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
